7d16dc1450b370ad3d2b28417fb32917deb96679
AnCamargo <ancamargob@gmail.com>
2024-05-08 22:48:30 -0500

    Change alembic heads

--- alembic/versions/01a9b87386de_create_all_tables.py
+++ alembic/versions/01a9b87386de_create_all_tables.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '01a9b87386de'
-down_revision: Union[str, None] = '50e134818e98'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
