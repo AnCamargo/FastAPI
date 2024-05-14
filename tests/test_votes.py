872062b985e4b253831b88052fddfe9d43a891ca
AnCamargo <ancamargob@gmail.com>
2024-05-13 19:26:21 -0500

    Finish test_votes.py and setup ci/cd
    Se finaliza los testing para test_votes.py y se crea la carpeta github/workflows que contiene el archivo build-deploy.yml para basicamente hacer el ci/cd de todo el proyecto, en el se establece que siempre que haya un push o PR en master se ejecuta, se crea una maquina ubuntu, se crean las variables de entorno, se crea el servicio de postgresse generan los testing con pytest y se setea la imagen de docker para hacer push a docker hub en el repositorio tambien se genera cache y se mueve el cache para que no siga creciendo, el cache es local

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -1,5 +1,36 @@
+import pytest
+from app import models, schema
+
+
+@pytest.fixture()
+def test_vote(test_posts, session, test_user):
+    new_vote = models.Votes(post_id=test_posts[2].id, user_id = test_user['id'], is_like = False)
+    session.add(new_vote)
+    session.commit()
+
 def test_vote_on_post(authorized_client, test_posts):
     response = authorized_client.post("/vote/", json={"post_id": test_posts[0].id, "vote_dir": 1})
     assert response.status_code == 200
 
-    
+def test_vote_on_post_already_voted(authorized_client, test_posts, test_vote):
+    response = authorized_client.post("/vote/", json={"post_id": test_posts[2].id, "vote_dir": 1})
+    assert response.status_code == 200
+    assert response.json()['is_like'] == True
+
+def test_delete_vote(authorized_client, test_posts, test_vote, test_user):
+    id_post = test_posts[2].id
+    response = authorized_client.post("/vote/", json={"post_id": test_posts[2].id, "vote_dir": 0})
+    assert response.status_code == 200
+    assert response.json()['content'] == f"Vote for the user email {test_user['email']} and post with id {id_post} have been deleted"
+
+def test_delete_vote_non_exist_dir_0(authorized_client, test_posts, test_user):
+    response = authorized_client.post("/vote/", json={"post_id": test_posts[2].id, "vote_dir": 0})
+    assert response.status_code == 406
+
+def test_delete_vote_non_exist_dir_diff_0(authorized_client, test_user):
+    response = authorized_client.post("/vote/", json={"post_id": 555, "vote_dir": 1})
+    assert response.status_code == 404
+
+def test_vote_unauthorized_user(client, test_posts):
+    response = client.post("/vote/", json={"post_id": test_posts[0].id, "vote_dir": 1})
+    assert response.status_code == 401
